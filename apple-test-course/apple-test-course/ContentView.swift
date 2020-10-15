5ad623ec7810ec1d7b0e0ed52795a8c16b69564b
Eli Levit <kalach2010@gmail.com>
2020-10-15 23:56:45 +0300

    first-commit

---
+++ apple-test-course/apple-test-course/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  apple-test-course
+//
+//  Created by Eli Levit on 15.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(Color.green)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple-test-course/apple-test-course/apple_test_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_test_courseApp.swift
+//  apple-test-course
+//
+//  Created by Eli Levit on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct apple_test_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
